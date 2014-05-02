40f819d88ac8abd1f02b0fcc8fd62d0b691df38c
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2014-05-02 07:57:00 +0200

    Added files

---
+++ main.js
@@ -0,0 +1,223 @@
+KEYS = {
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
+  P: 80,
+};
+
+WIDTH = 10;
+FPS = 30;
+SPEED_PER_SEC = 5;
+
+
+shapes = [
+  {
+    color: 'blue',
+
+    rotations: [
+
+      //   =
+      //   =
+      //   ==
+      function(ctx, x, y){
+        ctx.fillRect(x, y, WIDTH, WIDTH*2);
+        ctx.fillRect(x, y+(WIDTH*2), WIDTH*2, WIDTH);
+      },
+
+      //   ===
+      //   =
+      function(ctx, x, y){
+        ctx.fillRect(x, y, WIDTH*3, WIDTH);
+        ctx.fillRect(x, y+WIDTH, WIDTH, WIDTH);
+      },
+
+      //   ==
+      //    =
+      //    =
+      function(ctx, x, y){
+        ctx.fillRect(x, y, WIDTH*2, WIDTH);
+        ctx.fillRect(x+WIDTH, y+WIDTH, WIDTH, WIDTH*2);
+      },
+
+      //      =
+      //    ===
+      function(ctx, x, y){
+        ctx.fillRect(x+(WIDTH*2), y, WIDTH, WIDTH);
+        ctx.fillRect(x, y+WIDTH, WIDTH*3, WIDTH);
+      },
+    ]
+  },
+
+  {
+    color: 'yellow',
+
+    rotations: [
+
+      //   =
+      //   ==
+      //    =
+      function (ctx, x, y){
+        ctx.fillRect(x, y, WIDTH, WIDTH*2);
+        ctx.fillRect(x+WIDTH, y+WIDTH, WIDTH, WIDTH*2);
+      },
+
+      //    ==
+      //   ==
+      function (ctx, x, y){
+        ctx.fillRect(x, y, WIDTH*2, WIDTH);
+        ctx.fillRect(x+WIDTH, y+WIDTH, WIDTH*2, WIDTH);
+      },
+    ]
+  },
+
+  {
+    color: 'green',
+
+    rotations: [
+      //   =
+      //   ==
+      //   =
+      function (ctx, x, y){
+        ctx.fillRect(x, y, WIDTH, WIDTH*3);
+        ctx.fillRect(x+WIDTH, y+WIDTH, WIDTH, WIDTH);
+      },
+
+      //    =
+      //   ===
+      function (ctx, x, y){
+        ctx.fillRect(x+WIDTH, y, WIDTH, WIDTH);
+        ctx.fillRect(x, y+WIDTH, WIDTH*3, WIDTH);
+      },
+
+      //    =
+      //   ==
+      //    =
+      function (ctx, x, y){
+        ctx.fillRect(x, y+WIDTH, WIDTH, WIDTH);
+        ctx.fillRect(x+WIDTH, y, WIDTH, WIDTH*3);
+      },
+
+      //   ===
+      //    =
+      function (ctx, x, y){
+        ctx.fillRect(x, y, WIDTH*3, WIDTH);
+        ctx.fillRect(x+WIDTH, y+WIDTH, WIDTH, WIDTH);
+      }
+    ]
+  },
+
+  {
+    color: 'red',
+
+    rotations: [
+      //   =
+      //   =
+      //   =
+      //   =
+      function(ctx, x, y){
+        ctx.fillRect(x, y, WIDTH, WIDTH*4);
+      },
+
+      //   ====
+      function(ctx, x, y){
+        ctx.fillRect(x, y, WIDTH*4, WIDTH);
+      },
+    ]
+  },
+
+  {
+    color: 'orange',
+
+    rotations: [
+      //   ==
+      //   ==
+      function(ctx, x, y){
+        ctx.fillRect(x, y, WIDTH*2, WIDTH*2);
+      },
+    ]
+  }
+];
+
+
+function Piece(ctx, shape, x, y){
+  var currentRotationIndex = 0;
+
+  function rotate(){
+    console.log('rotate');
+    currentRotationIndex = (currentRotationIndex + 1) % shape.rotations.length;
+  }
+
+  function draw(){
+    ctx.fillStyle = shape.color;
+    shape.rotations[currentRotationIndex](ctx, x, y);
+  }
+
+  function move(mx, my){
+    x += mx;
+    y += my;
+    return y >= 400;
+  }
+
+  return {
+    draw: draw,
+    rotate: rotate,
+    move: move,
+  }
+}
+
+
+function main(){
+  var canvas = document.getElementById('tutorial');
+
+  var currentKey;
+  var currentShape = shapes.shift();
+
+
+
+  document.onkeydown = function(e){
+    currentKey = e.keyCode;
+    if (KEYS[currentKey]){return false;}
+  };
+  document.onkeyup = function(e){currentKey = undefined;};
+
+
+  if (canvas.getContext){
+    var ctx = canvas.getContext('2d');
+
+    x = 5;
+    y = 50;
+    s = 1;
+
+    function nextPiece(){
+      shapes.push(currentShape);
+      currentShape = shapes.shift();
+      currentPiece = Piece(ctx, currentShape, 10, 10);
+    }
+    nextPiece();
+
+    function draw(){
+      ctx.clearRect(0,0,1000, 600);
+
+      if (currentKey === KEYS.RIGHT){
+        currentPiece.move(WIDTH, 0);
+      } else if (currentKey === KEYS.LEFT){
+        currentPiece.move(-WIDTH, 0);
+      } else if (currentKey === KEYS.DOWN){
+        currentPiece.move(0, WIDTH);
+      } else if (currentKey === KEYS.UP){
+        currentPiece.rotate();
+        currentKey = undefined;
+      }
+      currentPiece.draw();
+    }
+    x = setInterval(draw, 1000/FPS);
+
+    setInterval(function(){
+      var isAtBottom = currentPiece.move(0, WIDTH);
+      if (isAtBottom){
+        nextPiece();
+      }
+    }, 1000/SPEED_PER_SEC);
+  }
+}
