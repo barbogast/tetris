6b2bcaebfbd30b0fc5f9ca1835ff18726efd2fac
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2014-05-02 08:25:22 +0200

    Enabled strict mode

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-KEYS = {
+"use strict";
+
+var KEYS = {
   LEFT: 37,
   UP: 38,
   RIGHT: 39,
@@ -6,12 +8,12 @@ KEYS = {
   P: 80,
 };
 
-WIDTH = 10;
-FPS = 30;
-SPEED_PER_SEC = 5;
+var WIDTH = 10;
+var FPS = 30;
+var SPEED_PER_SEC = 5;
 
 
-shapes = [
+var shapes = [
   {
     color: 'blue',
 
@@ -167,57 +169,80 @@ function Piece(ctx, shape, x, y){
 }
 
 
+var Game = function(ctx){
+  var currentKey;
+  var currentPiece;
+  var currentShape;
+
+  function nextPiece(){
+    shapes.push(currentShape);
+    currentShape = shapes.shift();
+    currentPiece = Piece(ctx, currentShape, 10, 10);
+  }
+
+  function draw(){
+    ctx.clearRect(0,0,1000, 600);
+
+    if (currentKey === KEYS.RIGHT){
+      currentPiece.move(WIDTH, 0);
+    } else if (currentKey === KEYS.LEFT){
+      currentPiece.move(-WIDTH, 0);
+    } else if (currentKey === KEYS.DOWN){
+      currentPiece.move(0, WIDTH);
+    } else if (currentKey === KEYS.UP){
+      currentPiece.rotate();
+      currentKey = undefined;
+    }
+    currentPiece.draw();
+  }
+
+  function keyDown(keyCode){
+    currentKey = keyCode;
+  }
+
+  function keyUp(){
+    currentKey = undefined;
+  }
+
+  function tick(){
+    var isAtBottom = currentPiece.move(0, WIDTH);
+    if (isAtBottom){
+      nextPiece();
+    }
+  }
+
+  currentShape = shapes.shift()
+  nextPiece();
+
+  return {
+    draw: draw,
+    keyDown: keyDown,
+    keyUp: keyUp,
+    tick: tick,
+  }
+}
+
+
 function main(){
+  var game;
   var canvas = document.getElementById('tutorial');
 
-  var currentKey;
-  var currentShape = shapes.shift();
-
-
-
   document.onkeydown = function(e){
-    currentKey = e.keyCode;
-    if (KEYS[currentKey]){return false;}
+    game.keyDown(e.keyCode);
+    if (KEYS[e.keyCode]){return false;}
   };
-  document.onkeyup = function(e){currentKey = undefined;};
+  document.onkeyup = function(e){game.keyUp()};
 
 
   if (canvas.getContext){
     var ctx = canvas.getContext('2d');
+    game = Game(ctx);
 
-    x = 5;
-    y = 50;
-    s = 1;
+    var x = 5;
+    var y = 50;
+    var s = 1;
 
-    function nextPiece(){
-      shapes.push(currentShape);
-      currentShape = shapes.shift();
-      currentPiece = Piece(ctx, currentShape, 10, 10);
-    }
-    nextPiece();
-
-    function draw(){
-      ctx.clearRect(0,0,1000, 600);
-
-      if (currentKey === KEYS.RIGHT){
-        currentPiece.move(WIDTH, 0);
-      } else if (currentKey === KEYS.LEFT){
-        currentPiece.move(-WIDTH, 0);
-      } else if (currentKey === KEYS.DOWN){
-        currentPiece.move(0, WIDTH);
-      } else if (currentKey === KEYS.UP){
-        currentPiece.rotate();
-        currentKey = undefined;
-      }
-      currentPiece.draw();
-    }
-    x = setInterval(draw, 1000/FPS);
-
-    setInterval(function(){
-      var isAtBottom = currentPiece.move(0, WIDTH);
-      if (isAtBottom){
-        nextPiece();
-      }
-    }, 1000/SPEED_PER_SEC);
+    setInterval(game.draw, 1000/FPS);
+    setInterval(game.tick, 1000/SPEED_PER_SEC);
   }
 }
